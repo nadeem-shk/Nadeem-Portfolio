3bc0ab1f9267257de49554d938eee89bd098ef8a
Shaikh Nadeem <shaikhnadeem84300@gmail.com>
2026-09-16 16:49:39 +0530

    Fix Skills import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Skills from "./components/Skills";
+import Skills from "./components/skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
